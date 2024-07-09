0e3946201ecfb051c2e47661d4cdfd0de2900357
yaoruchang <ruchangyao@gmail.com>
2024-07-09 11:24:47 +0800

    texture

--- stellara/core/app.js
+++ stellara/core/app.js
@@ -21,8 +21,22 @@ class Application {
         meshz.position.set(0, 0, 1);
         this.scene.add(meshz);
 
+        // light
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        this.scene.add(ambientLight);
+
+        const pointLight = new THREE.PointLight(0x0f0f0f, 1);
+        pointLight.position.set(celestialObjects[0].meshGroup.position);
+        // console.log(celestialObjects[0].meshGroup.position);
+        this.scene.add(pointLight);
+
         this.camera.position.set(0, 5, 0);
         this.camera.up.set(0, 0, 1);
+        var position = new THREE.Vector3(0, 0, 0);
+        this.celestialObjects[0].meshGroup.getWorldPosition(position);
+        console.log(position);
+        this.camera.lookAt(position);
+
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.enableDamping = true;
         this.controls.dampingFactor = 0.1;
